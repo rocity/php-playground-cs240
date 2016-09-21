00b0486d48178bd14429ab36a3e3155f8df67dee
derekviera <ma.dmviera01@gmail.com>
2016-09-21 16:47:05 -0400

    add randomvariables

---
+++ randomvariables.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>math</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.28" />
+	<style>
+	h1 {
+		text-align: center;
+		color: darkgreen;
+		font-size: 800%;
+	}
+	* {
+		background-color: lightgrey;
+	}
+	</style>
+</head>
+
+<body>
+	<h1><?php
+$x = rand (0, 100);
+
+$y = rand(0, 100);
+
+            print "$x + $y = ";
+
+print $x + $y;
+
+print "<br><br>";
+?> </h1>
+</body>
+
+</html>
