6d4add17ee0317b031f621521dc3d90b98943b50
derekviera <ma.dmviera01@gmail.com>
2016-10-21 20:07:07 -0400

    add hw7 (functions)

---
+++ projects/hw7.php
@@ -0,0 +1,14 @@
+<?php
+
+function signChange ($changeIt) {
+if ($changeIt < 0);
+return -$changeIt;
+} // end function
+
+$changeIt = $_REQUEST['changeIt'];
+
+$newNum = signChange($changeIt);
+print "<h3>The new number is: $newNum</h3>";
+    
+
+?>
